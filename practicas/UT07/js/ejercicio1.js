d0c0b2b6e852ab3d372aba3b881ae684a16d7df0
Juan Domínguez <juan.dompra@educa.jcyl.es>
2024-01-12 12:30:01 +0100

    U07-1-2

---
+++ practicas/UT07/js/ejercicio1.js
@@ -0,0 +1,18 @@
+const url = 'https://swapi.dev/api/planets';
+let json;
+let tabla = document.getElementById('list');
+
+async function conseguirDatos() {
+    fetch( url )
+        .then( response => response.json() )
+        .then( data =>  data.results.forEach(element => {
+            tabla.innerHTML+=`
+                <tr>
+                    <td>${element.name}</td>
+                    <td>${element.climate}</td>
+                    <td>${element.gravity}</td>
+                    <td>${element.terrain}</td>
+                </tr>
+            `}))};
+
+conseguirDatos();
